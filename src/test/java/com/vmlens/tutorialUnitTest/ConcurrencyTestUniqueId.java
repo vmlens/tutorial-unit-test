6232ea98c3ff9a0625731eeb9981b845ac74f1a4
Thomas Krieger <thomas.krieger@vmlens.com>
2019-07-09 16:53:04 +0200

    report examples

--- src/test/java/com/vmlens/tutorialUnitTest/ConcurrencyTestUniqueId.java
+++ src/test/java/com/vmlens/tutorialUnitTest/ConcurrencyTestUniqueId.java
@@ -13,14 +13,16 @@ public class ConcurrencyTestUniqueId {
 	
 	@Test
 	public void testUniqueId() throws InterruptedException {
-		
+		// Use two threads to call the method under test.
 		Thread first = new Thread( () ->    { firstId  = uniqueId.nextId();  } ) ;
 		Thread second = new Thread( () ->   { secondId = uniqueId.nextId();  } ) ;
+		// start the threads
 		first.start();
 		second.start();
+		// Wait till both threads stopped
 		first.join();
-		second.join();
-		
+		second.join();	
+		// check the result
 		assertTrue(  firstId != secondId );
 	}
 
